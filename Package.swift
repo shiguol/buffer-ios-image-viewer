e53745bdcc18cf56b5e2a8f5f61ca483f087b0af
shiguoliang <shiguoliang@modelbest.cn>
2024-06-21 17:02:53 +0800

    feat: update version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 let package = Package(
     name: "BFRImageViewer",
     defaultLocalization: "en",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         .library(
             name: "BFRImageViewer",
